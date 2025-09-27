d6c08cd7ae678a5b40a1934bec697bb23f906252
Brian <gamerfunky78@gmail.com>
2025-09-27 19:23:24 +0300

    add backend structure

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+
+
+const app = express();
+
+
+app.get( '/' , (req, res)=>{
+    res.json({message: "Hello from server!"})
+})
+
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`'Server is running on http://localhost:${process.env.PORT}'`);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
