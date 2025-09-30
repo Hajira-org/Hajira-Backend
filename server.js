ca0ce56c08930186442fb529a02feb97cc768269
Brian <gamerfunky78@gmail.com>
2025-09-30 13:54:50 +0300

    Update server configuration and environment variables for deployment

--- server.js
+++ server.js
@@ -5,27 +5,20 @@ const authRoutes = require("./routes/authRoutes");
 const cors = require("cors");
 const jobRoutes = require("./routes/jobRoutes");
 
+
 dotenv.config();
-
-const app = express();
-
-// Connect to MongoDB Atlas
 connectDB();
 
-// CORS
-app.use(cors({
-  origin: 'https://hajira-org.netlify.app', 
-}));
+const app = express();
+app.use(cors());
 app.use(express.json());
-
-// Routes
 app.use("/api/jobs", jobRoutes);
-app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hajira Backend API Running...");
 });
 
-// Use dynamic PORT for Render
-const PORT = process.env.PORT || 4000;
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
