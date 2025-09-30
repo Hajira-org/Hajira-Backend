3d0ac79c633487e34ad81cc394828a8767dc5044
Brian <gamerfunky78@gmail.com>
2025-09-30 14:58:43 +0300

    Refactor job routes to consolidate seeker job availability endpoint

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const { createJob, getJobs, updateJob, deleteJob, getAvailableJobs, applyJob } = require("../controllers/jobController");
 const { protect } = require("../middleware/authMiddleware");
 
+
+// Seeker job route
+router.get("/available", protect, getAvailableJobs);
+
 // Poster job routes
 router.post("/", protect, createJob);
 router.get("/", protect, getJobs);
@@ -11,7 +15,5 @@ router.delete("/:id", protect, deleteJob);
 router.post("/:id/apply", protect, applyJob); // seekers apply
 
 
-// Seeker job route
-router.get("/available", protect, getAvailableJobs);
 
 module.exports = router;
