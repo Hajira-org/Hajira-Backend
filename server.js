5c99f48bd6b1d29ef8570dea5b92432ebb5366b8
Brian <gamerfunky78@gmail.com>
2025-11-01 13:07:07 +0300

    Update CORS configuration to allow localhost and remove default port fallback

--- server.js
+++ server.js
@@ -47,7 +47,10 @@ const server = http.createServer(app);
 // --- Initialize Socket.IO ---
 const io = new Server(server, {
   cors: {
-    origin: ["hajira-org.netlify.app"], //  will be updated to my React frontend URL
+    origin: 
+    ["hajira-org.netlify.app",
+      "http://localhost:3000"
+    ], //  will be updated to my React frontend URL
     methods: ["GET", "POST"],
   },
 });
@@ -93,5 +96,5 @@ io.on("connection", (socket) => {
 
 
 // --- Start server ---
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
